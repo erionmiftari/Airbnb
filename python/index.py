da0c7b9add9682214b1de53338778d3d2df1ae7e
Erion Miftari <erion.miftari@umib.net>
2026-05-09 12:55:20 +0200

    Add demo-plan.md

---
+++ python/index.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+pesha = [164,158,172,153,144,156,189,163,134,159,143,176,177,162,141,151,182,185,171,152]
+mesatarja  = np.mean(pesha)
+mediana = np.mediana(pesha)
+devijimi std = np.std(pesha)
